2a788606b4df74601575e37ac5e798ba59a80b87
Arnor Nolen <andrey1gorbulin@gmail.com>
2023-09-06 22:50:09 +0300

    Updated icons

--- config.h
+++ config.h
@@ -25,7 +25,7 @@ static const char *const autostart[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "󰗊", "󰙯", "󰗃", "" };
+static const char *tags[] = { "", "", "", "", "", "", "󰙯", "", "" };
 
 static const Rule rules[] = {
     /* app_id     title       tags mask     isfloating   monitor */
